d2faaf8af13e5c6533f76891759ce557389b88ff
SUPORTE04 <SUPORTE04@SUPORTE04-PC>
2018-11-05 13:24:13 -0200

    Inicio classe CadCliente

--- src/seguro/view/ControlLogin.java
+++ src/seguro/view/ControlLogin.java
@@ -8,12 +8,14 @@ import seguro.model.Usuario;
  * @author Adston at self
  */
 public class ControlLogin {
-
    Usuario user = new Usuario();
 
+   
    public boolean Login( Conexao conexao, String[] dadosLogin ){
       String SQL = "select * from usuario where login = '" + dadosLogin[0] + "' and senha = '" + dadosLogin[1] + "' ;";
-      return this.user.Login( SQL, (DAOUsuario) conexao );
+      this.user.Login( SQL, (DAOUsuario) conexao );
+      
+   return ( dadosLogin[1].equalsIgnoreCase( this.user.getSenha()) );
    }
 
 
